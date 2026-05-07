beeb23d310e1944dac47beb747318e9399fe256d
Gutont <melofuto@gmail.com>
2026-05-07 13:35:46 -0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import vercel from "@tanstack/start-vercel";
 
 export default defineConfig({
-  adapters: [vercel()],
   tanstackStart: {
-    server: {
-      entry: "server",
-    },
+    server: { entry: "server" },
   },
 });
